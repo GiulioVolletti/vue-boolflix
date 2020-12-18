4047d49dae6a4c60e314a8b22ed1c3c5005d7faf
Giulio <giuliovolletti@hotmail.com>
2020-12-18 14:36:24 +0100

    commit4

--- js/script.js
+++ js/script.js
@@ -3,33 +3,37 @@ var flixApp = new Vue (
   {
     el: "#wrapper",
     data:{
-      textSearch:"",
+      textSearch: "",
       arraySearch:[],
 
     },
     methods:{
       searchClick: function() {
         console.log(flixApp.textSearch);
+        if (flixApp.textSearch != "") {
+          axios
+          .get( "https://api.themoviedb.org/3/search/movie/",{
+            params:{
+              api_key: "4a59e1623a8f16134b5fe205c4d4923e",
+              query: flixApp.textSearch,
+              language: "it-IT",
+            }
+          }
+        )
+        .then(
+          (element) => {
+            console.log(element.data.results);
+            this.arraySearch = element.data.results
+
+            console.log(this.arraySearch);
+          }
+        );
+
+        flixApp.textSearch = ""
+      };
       },
 
     },
-    mounted: function() {
-      console.log("hello world");
 
-      axios
-        .get( "https://api.themoviedb.org/3/search/movie/?api_key=4a59e1623a8f16134b5fe205c4d4923e&query=superman&language=it-IT")
-          .then(
-            (element) => {
-              console.log(element.data.results);
-              this.arraySearch = element.data.results
-
-              console.log(this.arraySearch);
-            }
-          );
-      // https://api.themoviedb.org/3/search/movie/
-
-      // https://api.themoviedb.org/3/movie/550?api_key=4a59e1623a8f16134b5fe205c4d4923e
-
-    },
   }
 );
