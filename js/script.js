29d5a071410e50606b8d350b81bb28293bf9de75
Giulio <giuliovolletti@hotmail.com>
2020-12-18 15:20:19 +0100

    commit7

--- js/script.js
+++ js/script.js
@@ -12,24 +12,24 @@ var flixApp = new Vue (
         console.log(flixApp.textSearch);
         if (flixApp.textSearch != "") {
           axios
-          .get( "https://api.themoviedb.org/3/search/movie/",{
-            params:{
-              api_key: "4a59e1623a8f16134b5fe205c4d4923e",
-              query: flixApp.textSearch,
-              language: "it-IT",
+            .get( "https://api.themoviedb.org/3/search/movie/",{
+              params:{
+                api_key: "4a59e1623a8f16134b5fe205c4d4923e",
+                query: flixApp.textSearch,
+                language: "it-IT",
+              }
             }
-          }
-        )
-        .then(
-          (element) => {
-            console.log(element.data.results);
-            this.arraySearch = element.data.results
+            )
+              .then(
+                (element) => {
+                  console.log(element.data.results);
+                  this.arraySearch = element.data.results
 
-            console.log(this.arraySearch);
-          }
-        );
+                  console.log(this.arraySearch);
+                }
+              );
 
-        flixApp.textSearch = ""
+          flixApp.textSearch = ""
       };
       },
 
